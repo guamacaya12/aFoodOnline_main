4cf4bc1fc6bc6f47ee3d52795b23359f95989554
guacamaya12 <tomas.zuppa@anahuac.mx>
2023-05-27 17:30:49 -0600

    modified:   aFoodOnline_main/__pycache__/settings.cpython-311.pyc 	modified:   aFoodOnline_main/settings.py 	new file:   aFoodOnline_main/static/css/bootstrap-datepicker.css 	new file:   aFoodOnline_main/static/css/bootstrap-slider.css 	new file:   aFoodOnline_main/static/css/bootstrap-theme.css 	new file:   aFoodOnline_main/static/css/bootstrap.css 	new file:   aFoodOnline_main/static/css/cs-foodbakery-plugin.css 	new file:   aFoodOnline_main/static/css/iconmoon.css 	new file:   aFoodOnline_main/static/css/style.css 	new file:   aFoodOnline_main/static/extra-images/Thumbs.db 	new file:   aFoodOnline_main/static/extra-images/banner-img.jpg 	new file:   aFoodOnline_main/static/extra-images/bg-image1.jpg 	new file:   aFoodOnline_main/static/extra-images/fb-restaurant-02.jpg 	new file:   aFoodOnline_main/static/extra-images/listing-logo18.png 	new file:   aFoodOnline_main/static/fonts/icomoon.eot 	new file:   aFoodOnline_main/static/fonts/icomoon.svg 	new file:   aFoodOnline_main/static/fonts/icomoon.ttf 	new file:   aFoodOnline_main/static/fonts/icomoon.woff 	new file:   aFoodOnline_main/static/images/PayPal.png 	new file:   aFoodOnline_main/static/images/Thumbs.db 	new file:   aFoodOnline_main/static/images/default-cover.PNG 	new file:   aFoodOnline_main/static/images/default-profile.PNG 	new file:   aFoodOnline_main/static/images/paid.png 	new file:   aFoodOnline_main/static/images/razorpay_logo.png 	new file:   aFoodOnline_main/static/js/bootstrap.js 	new file:   aFoodOnline_main/static/js/daterangepicker.js 	new file:   aFoodOnline_main/static/js/functions.js 	new file:   aFoodOnline_main/static/js/jquery-te-1.4.0.min.js 	new file:   aFoodOnline_main/static/js/jquery-ui.js 	new file:   aFoodOnline_main/static/js/jquery.js 	new file:   aFoodOnline_main/static/js/jquery.mCustomScrollbar.concat.min.js 	new file:   aFoodOnline_main/static/js/modernizr.js 	new file:   aFoodOnline_main/static/js/scripts.js 	new file:   aFoodOnline_main/static/js/tag-it.js 	new file:   aFoodOnline_main/static/js/theia-sticky-sidebar.js 	new file:   aFoodOnline_main/static/logo/favicon.png 	new file:   aFoodOnline_main/static/logo/foodOnlineLogo.png 	modified:   templates/home.html

--- aFoodOnline_main/settings.py
+++ aFoodOnline_main/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Configuración de archivos estáticos (html-css-js)
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES =[
+    'aFoodOnline_main/static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
